2af8790357fe1b3dfc86f043822e2852e899291d
Josue Alvarado <josue@vauxoo.com>
2021-02-02 14:19:47 -0600

    [REF] Add relational fields, and modify security for session

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -12,6 +12,7 @@ class Course(models.Model):
     responsible_id = fields.Many2one(
             'res.users', string="Responsible",
             index=True, ondelete='set null')
+    session_ids = fields.One2many('openacademy.session','course_id')
 
 class Session(models.Model):
     _name = 'openacademy.session'
@@ -23,3 +24,4 @@ class Session(models.Model):
     seats = fields.Integer(string="Number of seats")
     instructor_id = fields.Many2one('res.partner', string='Instructor')
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
