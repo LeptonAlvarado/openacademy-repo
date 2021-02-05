726c5f2367d67efe63b79f5c7d2889ca26880d35
Josue Alvarado <josue@vauxoo.com>
2021-02-05 13:02:51 -0600

    [REF] openacademy: Add report

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -28,7 +28,8 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/partner.xml'
+        'views/partner.xml',
+        'reports.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
